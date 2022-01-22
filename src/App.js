2a480bf4fcf3cc6d1454208ec2d1b645d8d1ddfd
Yami <yamilaveralopez@gmail.com>
2022-01-22 01:29:34 -0300

    add and delete cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card'
 
+
+
 const App = () => {
+
+  const [tarjetas, setTarjetas] = useState([])
+  let numeroTarjeta = 0
+
   // Se regresan elementos de JSX
-  const numero = 1;
+  let numero = 1;
 
 /*   const componente = {
     numero: numero
@@ -16,6 +22,10 @@ ListaAlumnosSimples.forEach((alumno, index) => {
   ListaJSX.push(<p key={index}>{alumno}</p>)
 })
 
+/*const tarjetas = []
+ for (let i = 0; i < 10; i++) {
+  tarjetas.push(<Card key={i} titulo={`La tarjeta numero ${i}`} numero={numero}/>)  
+} */
 
 /* const ListaAlumnos = [
   <p>Ivan</p>,
@@ -23,13 +33,27 @@ ListaAlumnosSimples.forEach((alumno, index) => {
   <p>Daniel</p>,
 ] */
 
+const agregarTarjeta = () => {
+  tarjetas.push(<Card key={tarjetas.length} titulo={`La tarjeta numero ${tarjetas.length}`} numero={numero}/>)
+  setTarjetas([...tarjetas])
+  numeroTarjeta++
+}
+
+const eliminarTarjeta = () => {
+  tarjetas.pop()
+  setTarjetas([...tarjetas])
+  numeroTarjeta--
+}
+
   return (
     <div>
       <p id='saludo'>Hola</p>
-      <Card titulo={`La tarjeta numero ${numero}`} numero={numero}/>
-      <Card titulo={`La tarjeta numero ${numero}`} numero={numero}/>
-      <Card titulo={`La tarjeta numero ${numero}`} numero={numero}/>
-      <Card titulo={`La tarjeta numero ${numero}`} numero={numero}/>
+      <h2>Prueba de css</h2>
+      <button onClick={agregarTarjeta}>Agregar tarjeta</button>
+      <button onClick={eliminarTarjeta}>Eliminar tarjeta</button>
+      <div className='cuadricula'>
+        {tarjetas}
+      </div>
       {/* {ListaAlumnos} */}
       
       {ListaJSX}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import '../css/Card.css'
 
 export default function Card(props) {
     const [numero, setNumero] = useState(0) // 5: valor inicial
@@ -9,9 +10,9 @@ export default function Card(props) {
         if(numero > 0) {
             setNumero(numero - 1)
         }
-
     }
-    return <>
+
+    return <div className='card'>
         {/* Esta entre llaves porque soy una expresion de JS */}
         <h2>{props.titulo}</h2>
         <p>Mi contenido {props.numero}</p>
@@ -20,5 +21,5 @@ export default function Card(props) {
         <button onClick={reducir}>Reducir</button>
         <button onClick={() => {setNumero(0)}}>Reset</button>
         {/* Mi comentario */}
-    </>;
+    </div>;
 }
